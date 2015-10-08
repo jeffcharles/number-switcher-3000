d4702a60228320b256c7bb4ee0b13c7f715a100c
Jeffrey Charles <jeffreycharles@gmail.com>
2015-10-07 21:34:53 -0400

    Simplify pure components

--- api/shared/components/error-alert.js
+++ api/shared/components/error-alert.js
@@ -2,14 +2,5 @@
 import Alert from 'react-bootstrap/lib/Alert';
 import React from 'react';
 
-export default class ErrorAlert extends React.Component {
-  render() {
-    return (
-      <Alert
-        bsStyle="danger"
-        onDismiss={this.props.onDismiss}>
-        {this.props.error.message || this.props.error}
-      </Alert>
-    );
-  }
-}
+export default ({ error, onDismiss }) =>
+  <Alert bsStyle="danger" onDismiss={onDismiss}>{error.message || error}</Alert>;
--- api/shared/components/loading.js
+++ api/shared/components/loading.js
@@ -1,8 +1,4 @@
 'use strict';
 import React from 'react';
 
-export default class Loading extends React.Component {
-  render() {
-    return <span>Loading...</span>;
-  }
-}
+export default () => <span>Loading...</span>;
