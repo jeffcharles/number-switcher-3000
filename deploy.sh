9d89fb9311e8c5143d921c99ba5dd04b987f7976
Jeffrey Charles <jeffreycharles@gmail.com>
2015-10-10 23:43:46 -0400

    See if using Dockerrun in zip works better

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 export AWS_DEFAULT_REGION=us-east-1
 
 VERSION=circle-${CIRCLE_SHA1}-$(date +%s)
-FILE=${VERSION}-Dockerrun.aws.json
+ARCHIVE=${VERSION}.zip
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker build -t jeffreycharles/number-switcher-3000:${VERSION} .
@@ -19,11 +19,12 @@ cat << EOF > Dockerrun.aws.json
 }
 EOF
 
-aws s3 cp Dockerrun.aws.json s3://number-switcher-3000-deployments/${FILE}
+zip $ARCHIVE Dockerrun.aws.json
+aws s3 cp Dockerrun.aws.json s3://number-switcher-3000-deployments/${ARCHIVE}
 aws elasticbeanstalk create-application-version \
   --application-name number-switcher-3000 \
   --version-label $VERSION \
-  --source-bundle S3Bucket='number-switcher-3000-deployments',S3Key="${FILE}"
+  --source-bundle S3Bucket='number-switcher-3000-deployments',S3Key="${ARCHIVE}"
 aws elasticbeanstalk update-environment \
   --environment-name number-switcher-3000 \
   --version-label $VERSION
