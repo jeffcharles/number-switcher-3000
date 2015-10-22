d1ee9b68893b41996dded3d84989c005dcb6119c
Jeffrey Charles <jeffreycharles@gmail.com>
2015-10-21 22:23:45 -0400

    Generate separate css bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   entry: './app/index.js',
   output: {
@@ -9,12 +11,19 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
-      { test: /\.woff2?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader')
+      },
+      {
+        test: /\.woff2?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
       { test: /\.ttf$/, loader: 'file-loader' },
       { test: /\.eot$/, loader: 'file-loader' },
       { test: /\.svg$/, loader: 'file-loader' }
     ]
   },
+  plugins: [new ExtractTextPlugin('bundle.css')],
   resolve: { root: __dirname }
 };
